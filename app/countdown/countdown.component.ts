994d9adbd6dd3372fb5f46be82c8e9662439febe
Bastien Signoret <bastien@1000mercis.com>
2016-06-03 18:45:07 +0200

    First implementation of countdown

--- app/countdown/countdown.component.ts
+++ app/countdown/countdown.component.ts
@@ -7,10 +7,36 @@ import {Router}            from '@angular/router-deprecated';
   styleUrls: ['app/countdown/countdown.component.css']
 })
 export class CountdownComponent implements OnInit {
+  countdown = 5;
+  private interval=null;
+  go = false;
 
   constructor(private router: Router) {
+  	var that=this;
+  	this.interval=setInterval(function() {that.decCountDown();}, 1000);
   }
 
   ngOnInit() {
+  }
+
+  decCountDown() {
+  	this.countdown--;
+  	if(this.countdown==0) {
+  		clearInterval(this.interval);
+  		this.showPicture();
+  	}  	
+  }
+
+  showPicture() {
+  	var that=this;
+  	this.go=true;
+  	setTimeout(function() {
+  		that.goToQuestion();
+  	},3000);
+  }
+
+  goToQuestion() {
+  	let link = ['Question'];
+    this.router.navigate(link);
   }
 }
