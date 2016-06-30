c06babac3c03ea801b7a757d6b59d72c5ae931b2
Pierre Guilbert <pierre@1000mercis.com>
2016-06-30 23:09:56 +0200

    Fix next attribute for new router

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,5 +6,6 @@ import {ROUTER_DIRECTIVES} from '@angular/router';
   template: '<router-outlet></router-outlet>',
   directives: [ROUTER_DIRECTIVES],
 })
+
 export class AppComponent {
 }
--- app/team/team.component.ts
+++ app/team/team.component.ts
@@ -1,13 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {}            from '@angular/router';
 
 @Component({
-  selector: 'quizz-country',
+  selector: 'quizz-team',
   templateUrl: 'app/team/team.component.html',
   styleUrls: ['app/team/team.component.css']
 })
-export class TeamComponent{
+export class TeamComponent {
+  team: Array<string>;
 
   constructor() {
+    this.team = {
+      'Ocito – Mobile': 'fa-mobile',
+      'Ocito – Back': 'fa-whatsapp',
+      'Front mobile': 'fa-android',
+      'Email': 'fa-envelope-o',
+      'Projets internes et outils': 'fa-gitlab',
+      'CRM – Data/BI': 'fa-sitemap',
+      'RTB – Data/BI': 'fa-calculator',
+      'RTB – Front - Back Ops': 'fa-envira',
+      'Data scientists': 'fa-pie-chart',
+      'Opés – Front': 'fa-apple',
+      'Opés – Back': 'fa-code',
+      'Sysadmin': 'fa-linux',
+    };
   }
 }
