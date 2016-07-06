3b27d5d59dc2f9226618db63a0a80dfa6a1bd66f
Florent Dancy <florent.dancy@1000mercis.com>
2016-07-06 23:51:48 +0200

    Add tag implementation for Question Page

--- app/question/question.component.ts
+++ app/question/question.component.ts
@@ -33,15 +33,43 @@ export class QuestionComponent implements OnInit {
         this.question = this.questions[this.counter];
         this.counter = this.counter + 1;
         this.questionService.save(this.question.id, index)
-            .subscribe(question => this.nextQuestion());
+            .subscribe(question => this.nextQuestion(question));
         if (this.counter === this.questions.length) {
             this.goResult();
         }
 
     }
 
-    nextQuestion() {
-        // callBack in here (_troq.push)
+    nextQuestion(question: any) {
+      if (!!question.data && question.data === true) {
+        (<any>window)._troq = (<any>window)._troq || [];
+
+        try {
+          var rtgpg = document.location.pathname.substr(1).split('/').join('-');
+          var rtganswer = 'true';
+        } catch (e) {
+          console.log(e);
+          var rtgpg = 'idontknowwhereyouaredudecomeonehelpmefindout';
+          var rtganswer = 'idontknowwhatsyouranswerdudecomeonehelpmefindout';
+        }
+
+        (<any>window)._troq.push(
+          ['tagid', '6562966-4ef7ac6eba09d1a17f777f2b8b8519b7'],
+          ['_rtganswer', rtganswer]
+        );
+
+        (function () {
+          if ((<any>window)._troqck !== 1) {
+            var a = document.createElement('script');
+            a.type = 'text/javascript';
+            a.async = !0;
+            a.src = '//mmtro.com/tro.js';
+            var b = document.getElementsByTagName('script')[0];
+            b.parentNode.insertBefore(a, b);
+          }
+        })();
+      }
+
     }
 
     ngOnInit() {
